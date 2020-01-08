14695431120d93f47076d35571d02d66d2df2b86
astrixial2 <56894285+astrixial2@users.noreply.github.com>
2020-01-08 15:17:36 -0600

    Update protocol.go

--- protocol.go
+++ protocol.go
@@ -1,1 +1,16 @@
 
+type resetColor() func()
+
+type startProver() func()
+
+type startVerifier() func()
+
+type startVerification() func()
+
+type startSpecs() func()
+
+type startCicleCount() func()
+
+func numBytesToString(size_t numBytes) {
+  
+}
